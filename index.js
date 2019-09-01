57bf390eb602fa279cda73f9556ff3a62bd5147e
Bots-private <54778689+Bots-private@users.noreply.github.com>
2019-09-01 22:45:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,124 @@
+const Discord = require('discord.js');
+const {
+	prefix,
+	token,
+} = require('./config.json');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const serverQueue = queue.get(message.guild.id);
+
+	if (message.content.startsWith(`${prefix}play`)) {
+		execute(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}skip`)) {
+		skip(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}stop`)) {
+		stop(message, serverQueue);
+		return;
+	} else {
+		message.channel.send('Введите правильную команду!')
+	}
+});
+
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
+
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('Вы должны быть в голосовом канале для проигрывания музыки!');
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+    return message.channel.send('У меня нету прав на это!');
+	}
+
+	const songInfo = await ytdl.getInfo(args[1]);
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+	};
+
+	if (!serverQueue) {
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} добавлена в список проигрывания.`);
+	}
+
+}
+
+function skip(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('Вы должны быть в голосовом чате чтобы пропустить песню!!');
+	if (!serverQueue) return message.channel.send('Нету песни чтобы пропустить ее!');
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('Вы должны быть в голосовом канале что остановить музыку!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('Ended');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+
+client.login("NjE3NzcwMTY1NzQ4MzY3Mzcx.XWv9iA.m4ixDtJD26XKe21XGTZrFfYuOsY");
